9ba397e876344759e14c2bf6126e3a0acbff3691
danielefongo <danielefongo@gmail.com>
2019-09-30 17:55:00 +0200

    log all the list changes for every card

--- main.js
+++ main.js
@@ -2,8 +2,26 @@ require('dotenv').config()
 var Trello = require("trello");
 var trello = new Trello(process.env.API_KEY, process.env.API_TOKEN);
 
+function trelloDate(dateString) {
+    return new Date(1000*parseInt(dateString.substring(0,8),16))
+}
+
 trello.getCardsOnBoard(process.env.BOARD_ID).then((cards) => {
     cards.forEach(element => {
-        console.log(element.shortLink)
+        trello.makeRequest('get', '/1/cards/' + element.shortLink + '/actions', { webhooks: true })
+        .then((activities) => {
+            activities = activities.reverse()
+            
+            console.log("Creation Date:" + trelloDate(element.id))
+            console.log(activities[0].data.listBefore)
+            console.log("----------------------------------------------------------------")
+            activities.forEach(activity => {
+                if(activity.type == "updateCard") {
+                    console.log("Activity Date:" + trelloDate(activity.id))
+                    console.log(activity.data.listAfter)
+                    console.log("----------------------------------------------------------------")
+                }
+            });
+        });
     });
 })
