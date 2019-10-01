3fb859e813e81cb6ae3351c4ba2802c4d264d4ee
danielefongo <danielefongo@gmail.com>
2019-10-01 19:24:13 +0200

    sum time to already added list

--- test/activities.js
+++ test/activities.js
@@ -10,7 +10,7 @@ describe('activities', () => {
 
   it('with one activity', async() => {
     let creationDate = new Date()
-    let oneSecondAfterCreationDate = new Date(creationDate.getTime() + 1000);
+    let oneSecondAfterCreationDate = datePlusSeconds(creationDate, 1);
 
     let activities = new Activities(creationDate)
     
@@ -20,8 +20,8 @@ describe('activities', () => {
 
   it('with two activities', async() => {
     let creationDate = new Date()
-    let oneSecondAfterCreationDate = new Date(creationDate.getTime() + 1000);
-    let twoSecondAfterCreationDate = new Date(creationDate.getTime() + 2000);
+    let oneSecondAfterCreationDate = datePlusSeconds(creationDate, 1);
+    let twoSecondAfterCreationDate = datePlusSeconds(creationDate, 2);
 
     let activities = new Activities(creationDate)
     
@@ -31,4 +31,23 @@ describe('activities', () => {
     expect(activities.get("oldList")).to.be.equal(1);
     expect(activities.get("newList")).to.be.equal(1);
   });
+
+  it('update activity by adding time', async() => {
+    let creationDate = new Date()
+    let oneSecondAfterCreationDate = datePlusSeconds(creationDate, 1);
+    let twoSecondAfterCreationDate = datePlusSeconds(creationDate, 2);
+    let threeSecondAfterCreationDate = datePlusSeconds(creationDate, 3);
+
+    let activities = new Activities(creationDate)
+    
+    activities.add({listBefore: "oldList", listAfter: "newList"}, oneSecondAfterCreationDate)
+    activities.add({listBefore: "newList", listAfter: "oldList"}, twoSecondAfterCreationDate)
+    activities.add({listBefore: "oldList", listAfter: "newList"}, threeSecondAfterCreationDate)
+    
+    expect(activities.get("oldList")).to.be.equal(2);
+  });
+
+  function datePlusSeconds(date, seconds) {
+    return new Date(date.getTime() + seconds * 1000)
+  }
 });
