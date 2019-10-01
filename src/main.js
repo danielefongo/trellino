0d982b9fa6b002fa45503cfa39aee00552d6cd48
danielefongo <danielefongo@gmail.com>
2019-10-01 21:36:43 +0200

    use activities module inside main

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 var Trello = require("trello");
+const Activities = require("./activities.js");
 var trello = new Trello(process.env.API_KEY, process.env.API_TOKEN);
 
 function trelloDate(dateString) {
@@ -30,36 +31,18 @@ trello.getCardsOnBoard(process.env.BOARD_ID).then((cards) => {
         trello.makeRequest('get', '/1/cards/' + card.shortLink + '/actions', { webhooks: true })
         .then((activities) => {
             activities = activities.reverse().filter((activity) => activity.type == "updateCard")
+            
+            var log = new Activities(trelloDate(card.id))
 
             if(activities.length === 0)
-                return
-            
-            var start = logObject(
-                activities[0].data.listBefore,
-                trelloDate(activities[0].id) - trelloDate(card.id))
+            return
 
-            var log = activities.map((activity, index) => {
-                if(activities[index + 1] == undefined)
-                    nextDate = new Date()
-                else
-                    nextDate = trelloDate(activities[index + 1].id)
-                return logObject(activity.data.listAfter, nextDate - trelloDate(activity.id))
-            });
-
-            log.unshift(start)
-
-            var times = {}
-            log.forEach(log => {
-                if(times[log.id] == undefined) {
-                    times[log.id] = {}
-                    times[log.id].time = 0
-                }
-                times[log.id].name = log.name
-                times[log.id].time += log.time
-            })
-
+            for(var i = 0; i < activities.length; i++) {
+                log.add(activities[i].data.listBefore.id, activities[i].data.listAfter.id, trelloDate(activities[i].id))
+            }
+        
             console.log("Card #" + card.idShort)
-            console.log(times)
+            console.log(log.getAll())
         });
     });
 })
